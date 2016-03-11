0538f1bcf79ff086cca79d4cdb1b538d2ea1ab5d
Mauricio Arellano <marellano@sciodev.com>
2016-03-11 17:51:32 -0600

    Added sign up page

---
+++ AppointmentControl/AppointmentControl/SignUpPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppointmentControl
+{
+    public partial class SignUpPage : ContentPage
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Save(object sender, EventArgs e)
+        {
+            //Application.Current.MainPage = new NavigationPage(new Page1()); 
+            Application.Current.MainPage = new Login();
+        }
+    }
+}
