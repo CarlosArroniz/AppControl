9400191b4ddd2364587bd38409ed91316f726066
CarlosArroniz <fredyarroniz@hotmail.com>
2016-03-14 11:30:25 -0600

    Added MyAppointments Page, Added Tabbed Page Example and Started Pages for tabs work

--- AppointmentControl/AppointmentControl/PrincipalPage.xaml.cs
+++ AppointmentControl/AppointmentControl/PrincipalPage.xaml.cs
@@ -1,18 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-
+﻿using Xamarin.Forms;
 namespace AppointmentControl
 {
     public partial class PrincipalPage : TabbedPage
     {
         public PrincipalPage()
         {
-            InitializeComponent();
+            this.ItemsSource = new NamedColor[]
+                                   {
+                                       new NamedColor("Today", Color.Green),
+                                       new NamedColor("Schedule", Color.Red),
+                                       new NamedColor("Settings", Color.Navy)
+                                   };
+            this.ItemTemplate = new DataTemplate(() => { return new NamedColorPage(); });
+            
+
+
+        }
+
+        class NamedColor
+        {
+            public NamedColor(string name, Color color)
+            {
+                this.Name = name;
+                this.Color = color;
+            }
+
+            public string Name { private set; get; }
+            public Color Color { private set; get; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        class NamedColorPage : ContentPage
+        {
+            public NamedColorPage()
+            {
+                this.SetBinding(ContentPage.TitleProperty, "Name");
+
+                BoxView boxView = new BoxView
+                                      {
+                                          WidthRequest = 100,
+                                          HeightRequest = 100,
+                                          HorizontalOptions = LayoutOptions.Center
+                                      };
+                boxView.SetBinding(BoxView.ColorProperty, "Color");
+
+                this.Content = boxView;
+            }
         }
     }
 }
