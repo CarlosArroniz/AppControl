7f3b1508e8928a09d039d2b72d7fa21743b280b1
CarlosArroniz <fredyarroniz@hotmail.com>
2016-03-15 09:43:37 -0600

    Added ListView for tabbed Page

--- AppointmentControl/AppointmentControl/MyAppointmentsPage.xaml.cs
+++ AppointmentControl/AppointmentControl/MyAppointmentsPage.xaml.cs
@@ -10,30 +10,93 @@ namespace AppointmentControl
 {
     public partial class MyAppointmentsPage : ContentPage
     {
-        
+
+        class Appointments
+        {
+            public Appointments(string name, DateTime date, string reason, Color color)
+            {
+                this.Name = name;
+                this.Date = date;
+                this.Reason = reason;
+                this.Color = color;
+            }
+
+            public string Name { private set; get; }
+
+            public DateTime Date { private set; get; }
+
+            public string Reason { private set; get; }
+            public Color Color { private set; get; }
+        };
 
         public MyAppointmentsPage()
         {
-            var Title = new Label()
-                            {
-                                Text = "My today Appointments"
-                            };
+            Label header = new Label
+                               {
+                                   Text = "Appointments for today:",
+                                   FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                                   HorizontalOptions = LayoutOptions.Center
+                               };
 
-            var Entry = new Entry() { Placeholder = "I am an entry", BackgroundColor = Color.White };
+            List<Appointments> appointments = new List<Appointments>
+                                                  {
+                                                      new Appointments("Mr. Carlos Arroniz", new DateTime(2016,03,20,10,00,00,00), "Chronic headache", Color.FromHex("#ee9c00")),
+                                                      new Appointments("Mrs. Beatriz Paredes", new DateTime(2016,03,20,11,30,00), "Painfull pain in legs xD", Color.FromHex("#f00"))
+                                                  };
 
-            Content = new StackLayout()
-                            {
-                                Orientation = StackOrientation.Vertical,
-                                BackgroundColor = Color.FromHex("#12A5F4"),
-                                WidthRequest = 100,
-                                HeightRequest = 100,
-                                Children = { Title, Entry }
-                            };
-        }
+            ListView appointsList = new ListView
+                                        {
+                                            ItemsSource = appointments,
 
-        async void OnNextPageButtonClicked(object sender, TappedEventArgs e)
-        {
-            await Navigation.PushAsync(new MyProfilePage());
+                                            ItemTemplate = new DataTemplate(() =>
+                                                    {
+                                                        Label nameLabel = new Label();
+                                                        nameLabel.SetBinding(Label.TextProperty, "Name");
+
+                                                        Label dateLabel = new Label();
+                                                        dateLabel.SetBinding(Label.TextProperty, new Binding("Date", BindingMode.OneWay, null, null, "Appointment Date {0:d}"));
+
+                                                        Label reasonLabel = new Label();
+                                                        reasonLabel.SetBinding(Label.TextProperty, "Reason");
+
+                                                        BoxView boxView = new BoxView();
+                                                        boxView.SetBinding(BoxView.ColorProperty, "Color");
+
+                                                        return new ViewCell
+                                                                   {
+                                                                       View = new StackLayout
+                                                                                  {
+                                                                                      Padding = 10,
+                                                                                      Orientation = StackOrientation.Horizontal,
+                                                                                      Children =
+                                                                                          {
+                                                                                              boxView, new StackLayout
+                                                                                                           {
+                                                                                                               BackgroundColor = Color.Green,
+                                                                                                               VerticalOptions = LayoutOptions.Center,
+                                                                                                               Spacing = 10,
+                                                                                                               Children =
+                                                                                                                   {
+                                                                                                                       nameLabel,
+                                                                                                                       dateLabel,
+                                                                                                                       reasonLabel
+                                                                                                                   }
+                                                                                                           }
+                                                                                          }
+                                                                                  }
+                                                                   };
+                                                    })
+                                        };
+            this.Padding = new Thickness(10, Device.OnPlatform(20,0,0), 10, 5);
+
+            this.Content = new StackLayout
+                               {
+                                   Children =
+                                       {
+                                           header, appointsList
+                                       }
+                               };
+
         }
     }
 }
--- AppointmentControl/AppointmentControl/PrincipalPage.xaml.cs
+++ AppointmentControl/AppointmentControl/PrincipalPage.xaml.cs
@@ -5,56 +5,13 @@ namespace AppointmentControl
     {
         public PrincipalPage()
         {
-            this.ItemsSource = new NamedColor[]
-                                   {
-                                       new NamedColor("Today", Color.Green),
-                                       new NamedColor("Schedule", Color.Red),
-                                       new NamedColor("Settings", Color.Navy)
-                                   };
-            this.ItemTemplate = new DataTemplate(() => { return new NamedColorPage(); });
-            
-
-
+            Children.Add(new MyAppointmentsPage { Title = "My Appointments" });
+            Children.Add(new CreateAppointment { Title = "Create Appointment" });
+            Children.Add(new MyProfilePage { Title = "My Profile" });
         }
-
         public void CreateAppointment()
         {
             Application.Current.MainPage = new CreateAppointment();
-        }
-
-        class NamedColor
-        {
-            public NamedColor(string name, Color color)
-            {
-                this.Name = name;
-                this.Color = color;
-            }
-
-            public string Name { private set; get; }
-            public Color Color { private set; get; }
-
-            public override string ToString()
-            {
-                return Name;
-            }
-        }
-
-        class NamedColorPage : ContentPage
-        {
-            public NamedColorPage()
-            {
-                this.SetBinding(ContentPage.TitleProperty, "Name");
-
-                BoxView boxView = new BoxView
-                                      {
-                                          WidthRequest = 100,
-                                          HeightRequest = 100,
-                                          HorizontalOptions = LayoutOptions.Center
-                                      };
-                boxView.SetBinding(BoxView.ColorProperty, "Color");
-
-                this.Content = boxView;
-            }
         }
     }
 }
