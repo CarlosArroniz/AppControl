d7250c6206879d3086cb42dea620872df28b1a87
Mauricio Arellano <marellano@sciodev.com>
2016-03-17 17:59:37 -0600

    added user manager class

---
+++ AppointmentControl/AppointmentControl/Data/UserManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AppointmentControl.Models;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace AppointmentControl.Data
+{
+    public class UserManager
+    {
+        // Azure
+        private readonly IMobileServiceTable<User> _table;
+        private readonly MobileServiceClient _azureClient;
+
+        public UserManager()
+        {
+            _azureClient = new MobileServiceClient(
+                Constants.AzureApplicationUrl,
+                Constants.AzureApplicationKey);
+
+            _table = _azureClient.GetTable<User>();
+        }
+
+        public async Task<User> GetTaskAsync(string id)
+        {
+            try
+            {
+                return await _table.LookupAsync(id);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"INVALID {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"ERROR {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task<ObservableCollection<User>> GetTodoItemsAsync()
+        {
+            try
+            {
+                return new ObservableCollection<User>(
+                    await _table.Where(user => user.Username != null).ToListAsync());
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"INVALID {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"ERROR {0}", e.Message);
+            }
+            return null;
+        }
+
+        public async Task SaveTaskAsync(User user)
+        {
+            if (user.Id == null)
+            {
+                await _table.InsertAsync(user);
+                //TodoViewModel.TodoItems.Add(user);
+            }
+            else
+            {
+                await _table.UpdateAsync(user);
+            }
+        }
+    }
+}
