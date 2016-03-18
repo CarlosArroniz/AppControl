cb29e39fe3a38b4af956e4acda31b8f63f90af51
CarlosArroniz <fredyarroniz@hotmail.com>
2016-03-18 09:16:35 -0600

    Tiny Changes

---
+++ AppointmentControl/AppointmentControl/EditProfileO/EditAccount.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppointmentControl.EditProfileOptions
+{
+    public partial class EditAccount : ContentPage
+    {
+        public EditAccount()
+        {
+            InitializeComponent();
+
+            var usernameLabel = new Label { };
+        }
+    }
+}
