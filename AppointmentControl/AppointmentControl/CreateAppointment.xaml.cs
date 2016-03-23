2ae4cbdd77b4a0c8d8c63f169a2313305df5a822
Mauricio Arellano <marellano@sciodev.com>
2016-03-23 13:02:53 -0600

    save appointment with dummy id's data

--- AppointmentControl/AppointmentControl/CreateAppointment.xaml.cs
+++ AppointmentControl/AppointmentControl/CreateAppointment.xaml.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AppointmentControl.Data;
 using AppointmentControl.Models;
 using Xamarin.Forms;
 
@@ -6,28 +11,85 @@ namespace AppointmentControl
 {
     public partial class CreateAppointment : ContentPage
     {
+        private readonly DoctorManager doctorManager;
+        private readonly PatientManager patientManager;
+        private readonly AppointmentManager appointmentManager;
+
         public CreateAppointment()
         {
             InitializeComponent();
+            doctorManager = new DoctorManager();
+            patientManager = new PatientManager();
+            appointmentManager = new AppointmentManager();
         }
 
-        private void Save(object sender, EventArgs e)
+        private async void Save(object sender, EventArgs e)
         {
-            CreateAppointmentAsDoctor();
-            CreateAppointmentAsPatient();
+            try
+            {
+                Appointment appointment = await CreateNewAppointment();
+                await appointmentManager.SaveTaskAsync(appointment);
 
-            this.BackgroundColor = Color.FromHex("#FFF");
-            Application.Current.MainPage = new PrincipalPage();
+                this.BackgroundColor = Color.FromHex("#FFF");
+                Application.Current.MainPage = new PrincipalPage();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"ERROR {0}", ex.Message);
+                Debug.WriteLine(@"ERROR {0}", ex);
+            }
         }
 
-        private void CreateAppointmentAsPatient()
+        private async Task<Appointment> CreateNewAppointment()
         {
+            bool isDoctor = true;
+            Appointment appointment = null;
+            if (isDoctor)
+            {
+                appointment = await CreateAppointmentAsDoctor();
+            }
+            else
+            {
+                appointment = await CreateAppointmentAsPatient();
+            }
+
+            appointment.Reason = Reason.Text;
+            string appointmentStartDate = GetTimestamp(Date.Date, StartHour.Time);
+            string appointmentEndDate = GetTimestamp(Date.Date, EndHour.Time);
+            appointment.StartDate = appointmentStartDate;
+            appointment.EndDate = appointmentEndDate;
+
+            return appointment;
+
+        }
+
+        private string GetTimestamp(DateTime date, TimeSpan time)
+        {
+            return date.ToString("yyyy-MM-dd") + "T" + time.ToString("hh\\:mm\\:ss\\.ffff") + "+00:00";
+        }
+
+        private async Task<Appointment> CreateAppointmentAsPatient()
+        {
+            //List<Patient> patientResult = await patientManager.FindPatientByName(PatientName.Text);
+            
+            return new Appointment()
+            {
+                DoctorId = "doctor_id", //change for global value
+                status = Appointment.ACCEPTED,
+              //  PatientId = patientResult.First().Id
+                PatientId = "dummy"
+            };
             throw new NotImplementedException();
         }
 
-        private void CreateAppointmentAsDoctor()
+        private async Task<Appointment> CreateAppointmentAsDoctor()
         {
-            throw new NotImplementedException();
+            return new Appointment()
+            {
+                PatientId = "patient_id", //change for global value
+                status = Appointment.REQUESTED,
+                DoctorId = "doctor_id" // get the doctor object or doctor_id before create the appointment
+            };
         }
     }
 }
