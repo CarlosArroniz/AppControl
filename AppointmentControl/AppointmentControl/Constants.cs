5e6e0f1d8af05061dc25ac470186b60fe1deeb6d
Mauricio Arellano <marellano@sciodev.com>
2016-03-24 09:04:13 -0600

    added constant for global property name

--- AppointmentControl/AppointmentControl/Constants.cs
+++ AppointmentControl/AppointmentControl/Constants.cs
@@ -1,10 +1,13 @@
 ﻿namespace AppointmentControl
 {
-    public class Constants
+    public abstract class Constants
     {
         // Sign up for an Azure account at https://account.windowsazure.com/signup
         // Replace strings with your mobile services url and key.
         public static string AzureApplicationUrl = @"https://appointmentcontrol.azure-mobile.net/";
         public static string AzureApplicationKey = @"kaKHoQLgVKFAzPgPoVKSKeceGXXvNY61"; 
+
+        // Constants for application
+        public static string UserPropertyName = "user";
     }
 }
