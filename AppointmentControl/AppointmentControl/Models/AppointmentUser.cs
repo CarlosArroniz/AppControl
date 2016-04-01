d17b8c2d09f4834570414f85e0fb9bc718ab4e17
Mauricio Arellano <marellano@sciodev.com>
2016-04-01 09:21:04 -0600

    Wrapper class for appointments

---
+++ AppointmentControl/AppointmentControl/Models/AppointmentUser.cs
@@ -0,0 +1,15 @@
+﻿namespace AppointmentControl.Models
+{
+    public class AppointmentUser
+    {
+        public string Id { get; set; }
+        public string DoctorId { get; set; }
+        public string PatientId { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Reason { get; set; }
+        public string status { get; set; } 
+        public string DoctorName { get; set; } 
+        public string PatientName { get; set; } 
+    }
+}
